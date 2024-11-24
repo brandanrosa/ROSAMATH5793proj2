c99472b1b706cca4ea65ad6ffc854b52ad56ee03
Brandan Rosa <brandanrosa22@gmai.com>
2024-11-23 20:09:34 -0600

    MICROWAVE data set added

---
+++ R/micro.R
@@ -0,0 +1,13 @@
+#' MICROWAVE
+#'
+#' A data set of the amount of radiation that 42 microwave ovens emit with the door closed and open
+#'
+#' @format
+#' A data frame with 42 rows and 2 columns:
+#' \describe{
+#'   \item{Closed}{Radiation measures with door closed}
+#'   \item{Open}{Radiation measures with door open}
+#'   ...
+#' }
+#' @source https://www.amazon.com/Multivariate-Statistical-Analysis-Classics-Statistics...
+"micro"
