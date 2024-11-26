2724605d798559d5c7ae7c497528e6b36c7dd360
Brandan Rosa <brandanrosa22@gmai.com>
2024-11-26 14:51:24 -0600

    Last two functions

---
+++ R/LANGUAGE.R
@@ -0,0 +1,22 @@
+#' LANGUAGE
+#'
+#' A data set of concordant first letters for numbers in 11 languages
+#'
+#' @format
+#' A data frame with 11 rows and 11 columns:
+#' \describe{
+#'   \item{E}{English}
+#'   \item{N}{Norwegian}
+#'   \item{Da}{Danish}
+#'   \item{Du}{Dutch}
+#'   \item{G}{German}
+#'   \item{Fr}{French}
+#'   \item{Sp}{Spanish}
+#'   \item{I}{Itialian}
+#'   \item{P}{Polish}
+#'   \item{H}{Hungarian}
+#'   \item{Fi}{Finnish}
+#'   ...
+#' }
+#' @source https://www.amazon.com/Multivariate-Statistical-Analysis-Classics-Statistics...
+"LANGUAGE"
---
+++ R/maleturt.R
@@ -0,0 +1,14 @@
+#' maleturt
+#'
+#' A data set of measurements of 24 male turtles' carapaces
+#'
+#' @format
+#' A data frame with 24 rows and 3 columns:
+#' \describe{
+#'   \item{LENGTH}{Legnth of carapace}
+#'   \item{WIDTH}{Width of carapace}
+#'   \item{HEIGHT}{Height of carapace}
+#'   ...
+#' }
+#' @source <https://www.amazon.com/Statistics-Engineering-Sciences-William...
+"maleturt"
